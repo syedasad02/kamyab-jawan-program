7b116277c6faed9bb2a60736263e1157d7c0e488
syedasad02 <74541873+syedasad02@users.noreply.github.com>
2020-12-03 17:02:22 +0500

    arduino work

---
+++ Hamza_arduino_1st_progtam.ino
@@ -0,0 +1,13 @@
+#define led 13
+bool a;
+void setup() {
+  // put your setup code here, to run once;
+pinMode(led, OUTPUT);
+pinMode(2, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly
+ a-digitalRead(2); 
+digitalWrite (led,a);
+}
