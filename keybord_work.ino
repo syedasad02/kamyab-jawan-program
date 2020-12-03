b4980197944cb49f5a24037f83065effb7533f9e
syedasad02 <74541873+syedasad02@users.noreply.github.com>
2020-12-03 18:32:23 +0500

    Add files via upload

---
+++ keybord_work.ino
@@ -0,0 +1,18 @@
+char a;
+void setup () {
+  // put your setup code here,to run once; 
+  pinMode (13, OUTPUT);
+  Serial.begin(9600);
+}
+void loop () {
+  char a=Serial.read();
+  // put your main code here , to run repeatedly;
+  if(Serial.available())
+  a=digitalRead(13);
+  if (a=='1')
+  {digitalWrite(13,HIGH);}
+  else if(a=='2')
+
+ { digitalWrite(13,LOW);}
+}
+
